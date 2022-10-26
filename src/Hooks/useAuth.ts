d3ea5ba90b40c68d6fc8cdcfc2903365d2f1fabd
Santiago Siordia <santiago.gonzalez@wizeline.com>
2022-10-26 16:36:44 -0500

    feat: useAuth hook

---
+++ src/Hooks/useAuth.ts
@@ -0,0 +1,18 @@
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { useEffect, useState } from 'react';
+
+const auth = getAuth();
+
+export const useAuth = () => {
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      user ? setUser(user) : setUser(undefined);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return user;
+}
