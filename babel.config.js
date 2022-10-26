df52df81a678bf33206275052b65813b688d339e
Santiago Siordia <santiago.gonzalez@wizeline.com>
2022-10-26 16:32:25 -0500

    feat: dotenv

--- babel.config.js
+++ babel.config.js
@@ -15,6 +15,13 @@ module.exports = function(api) {
           },
         },
       ],
+      [
+        "module:react-native-dotenv",
+        {
+            moduleName: "@env",
+            path: ".env",
+        },
+    ],
     ],
   };
 };
