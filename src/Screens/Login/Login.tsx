7bed75db71cb45e3110c849aa1f1d9f0d56a0388
Santiago Siordia <santiago.gonzalez@wizeline.com>
2022-10-26 19:55:02 -0500

    feat: login design

--- src/Screens/Login/Login.tsx
+++ src/Screens/Login/Login.tsx
@@ -1,4 +1,6 @@
 import { BText } from "@components";
+import { useAppNavigation } from "@hooks";
+import { ColorsEnum } from "@theme";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useFormik } from 'formik';
 import React, { FC } from "react";
@@ -7,6 +9,8 @@ import { Pressable, StyleSheet, TextInput, View } from "react-native";
 const auth = getAuth();
 
 export const Login: FC = () => {
+
+  const { goBack } = useAppNavigation();
 
   const { handleChange, handleBlur, values, handleSubmit } = useFormik({
     initialValues: {
@@ -25,7 +29,7 @@ export const Login: FC = () => {
   });
 
   return <View style={styles.loginContainer}>
-    <BText>Log in Screen</BText>
+    <BText size="large" color="black">Ingresa a tu cuenta</BText>
     <TextInput
       placeholder="email"
       onChangeText={handleChange('email')}
@@ -38,8 +42,16 @@ export const Login: FC = () => {
       onBlur={handleBlur('password')}
       value={values.password}
     />
-    <Pressable onPress={() => handleSubmit()}>
-      <BText>Log in</BText>
+    <Pressable style={styles.loginButton} onPress={() => handleSubmit()}>
+      <BText color="white">Ingresar</BText>
+    </Pressable>
+    <View style={{ height: 8 }}/>
+    <Pressable style={styles.backButton} onPress={() => handleSubmit()}>
+      <BText color="secondary">Crear una cuenta</BText>
+    </Pressable>
+    <View style={{ height: 16 }}/>
+    <Pressable onPress={goBack}>
+      <BText color="secondary">Regresar</BText>
     </Pressable>
   </View>;
 }
@@ -47,6 +59,25 @@ export const Login: FC = () => {
 const styles = StyleSheet.create({
   loginContainer: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: ColorsEnum.backgroundSecondary,
+    padding: 16
+  },
+  loginButton: {
+    backgroundColor: ColorsEnum.secondary,
+    padding: 8,
+    borderRadius: 8,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  backButton: {
+    borderColor: ColorsEnum.secondary,
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 8,
+    width: "100%",
     justifyContent: "center",
     alignItems: "center",
   },
