4b01a953471230dfb9dff42a4e0e972835015c23
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2022-12-21 19:18:06 -0600

    fix: safe area insets apply to others profile too

--- src/Screens/OtherProfile/OtherProfile.tsx
+++ src/Screens/OtherProfile/OtherProfile.tsx
@@ -3,6 +3,7 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { ColorsEnum } from "@theme";
 import React, { FC } from "react";
 import { Pressable, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { users } from "../../Data/Users";
 
 export type OtherProfileProps = NativeStackScreenProps<RequestStackParamList, 'OtherProfile'>;
@@ -18,31 +19,31 @@ export const OtherProfile: FC<OtherProfileProps> = ({
 
   if(!user) return null
 
-  return <View style={styles.container}>
-  <ProfileImage avatar={user.avatar}/>
-  <BText size="title" superBold color="secondary">{user.name}, {user.age}</BText>
-  <View style={styles.divider} />
-  
-  
-  <ProfileData
-    bloodType={user.bloodType}
-    location={user.location}
-    litersDonated={user.litersDonated}
-    contact={user.contact}
-  />
+  return <SafeAreaView style={styles.container}>
+    <ProfileImage avatar={user.avatar}/>
+    <BText size="title" superBold color="secondary">{user.name}, {user.age}</BText>
+    <View style={styles.divider} />
+    
+    
+    <ProfileData
+      bloodType={user.bloodType}
+      location={user.location}
+      litersDonated={user.litersDonated}
+      contact={user.contact}
+    />
 
-  <View style={styles.divider} />
-  <View>
-    <BText size="title" bold color="secondary">About</BText>
-    <BText color="black" style={{ marginTop: 8 }}>{user.description}</BText>
-  </View>
+    <View style={styles.divider} />
+    <View>
+      <BText size="title" bold color="secondary">About</BText>
+      <BText color="black" style={{ marginTop: 8 }}>{user.description}</BText>
+    </View>
 
-  <View style={styles.buttonSection}>
-    <Pressable style={styles.contactButton} onPress={() => console.log("Contact")}>
-      <BText color="white" bold>Contact</BText>
-    </Pressable>
-  </View>
-</View>
+    <View style={styles.buttonSection}>
+      <Pressable style={styles.contactButton} onPress={() => console.log("Contact")}>
+        <BText color="white" bold>Contact</BText>
+      </Pressable>
+    </View>
+  </SafeAreaView>
 }
 
 const styles = StyleSheet.create({
