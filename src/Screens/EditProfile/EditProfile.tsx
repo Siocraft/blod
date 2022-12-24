4b6c9ecec2a6b73fe0b63a6f38b48668c8da3774
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2022-12-24 16:36:07 -0600

    feat: bloodtype initial value

--- src/Screens/EditProfile/EditProfile.tsx
+++ src/Screens/EditProfile/EditProfile.tsx
@@ -2,7 +2,7 @@ import { BText, BTextInput, ProfileImage } from "@components";
 import { useAuth, useUser } from "@hooks";
 import { useFormik } from "formik";
 import { FC } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ErrorScreen } from "../Error";
 import { Loading } from "../Loading";
@@ -16,22 +16,28 @@ export const EditProfile: FC = () => {
     isError: isErrorUser,
   } = useUser(authUser?.uid);
 
-  const { handleChange, handleBlur, values, handleSubmit, errors, touched } =
+  const { handleChange, handleBlur, values, handleSubmit, errors, touched, setFieldValue } =
     useFormik({
       initialValues: {
-        password: "",
+        bloodType: user?.bloodType,
       },
-      onSubmit: values => {
-        console.log(values);
+      onSubmit: submittedValues => {
+        console.log(submittedValues);
       },
     });
 
+  if(values.bloodType === undefined && user?.bloodType) setFieldValue('bloodType', user.bloodType);
+
   if (!authUser) return <GuestSignedIn />;
   if (isLoadingUser) return <Loading />;
-  if (!user || isErrorUser) return <ErrorScreen />;
+  if (!user || isErrorUser) return <ErrorScreen />
 
-  const passwordError =
-    !!errors.password && !!touched.password && !!values.password;
+  const bloodTypeError =
+    !!errors.bloodType && !!touched.bloodType && !!values.bloodType;
+
+  const onPressSave = () => {
+    handleSubmit();
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -43,15 +49,16 @@ export const EditProfile: FC = () => {
       <BTextInput value={authUser.email ?? ""} disabled />
       <View style={{ height: 8 }} />
       <BTextInput
-        secureTextEntry
-        placeholder="Constraseña"
-        onChangeText={handleChange("password")}
-        onBlur={handleBlur("password")}
-        value={values.password}
-        error={passwordError}
-        errorMessage={errors.password}
+        placeholder="Tipo de Sangre"
+        onChangeText={handleChange("bloodType")}
+        onBlur={handleBlur("bloodType")}
+        value={values.bloodType}
+        error={bloodTypeError}
       />
       <View style={{ height: 8 }} />
+      <Pressable onPress={onPressSave} >
+        <BText color="primary">Guardar cambios</BText>
+      </Pressable>
     </SafeAreaView>
   );
 };
