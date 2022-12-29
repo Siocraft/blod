67e959cb9567bb4870a5367e89e39fe9bf42e4b9
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2022-12-29 11:14:56 -0600

    feat: pressable camera

--- src/Components/ProfileImage/ProfileImage.tsx
+++ src/Components/ProfileImage/ProfileImage.tsx
@@ -1,17 +1,20 @@
 import { ColorsEnum } from "@theme";
 import React, { FC } from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, Pressable, StyleSheet, View } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export interface ProfileImageProps {
   avatar: string;
   size?: number;
   borderRadius?: number;
+  editable?: boolean;
 }
 
 export const ProfileImage: FC<ProfileImageProps> = ({
   avatar,
   size = 200,
   borderRadius = 8,
+  editable = false,
 }) => {
   const imageContainerStyle = StyleSheet.flatten([
     styles.imageContainer,
@@ -21,6 +24,10 @@ export const ProfileImage: FC<ProfileImageProps> = ({
       borderRadius: (size + borderRadius) / 2,
     },
   ]);
+
+  const onEditImage = () => {
+    console.log("Edit Image");
+  };
 
   return (
     <View style={imageContainerStyle}>
@@ -34,6 +41,9 @@ export const ProfileImage: FC<ProfileImageProps> = ({
           borderRadius: size / 2,
         }}
       />
+      <Pressable onPress={onEditImage} style={styles.cameraContainer}>
+        <MaterialIcons name="camera-alt" size={32} color="white" />
+      </Pressable>
     </View>
   );
 };
@@ -45,4 +55,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginBottom: 16,
   },
+  cameraContainer: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    backgroundColor: ColorsEnum.secondary,
+    borderRadius: 100,
+    padding: 8,
+  }
 });
