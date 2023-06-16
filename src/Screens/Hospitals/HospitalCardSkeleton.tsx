c944e264d61fb2bb5c8ec865504e55d41aed1eda
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2023-06-16 10:01:22 -0600

    feat: hospital skeleton card

---
+++ src/Screens/Hospitals/HospitalCardSkeleton.tsx
@@ -0,0 +1,65 @@
+import { BButton, Skeleton } from "@components";
+import { ColorsEnum } from "@theme";
+import { FC } from "react";
+import { View, StyleSheet } from "react-native";
+
+export const HospitalCardSkeleton: FC = () => {
+
+  return <View style={styles.cardContainer}>
+    <Skeleton variant="text" />
+    <View style={{ height: 8 }} />
+    <Skeleton variant="text" style={{ width: '70%' }} />
+    <View style={{ height: 16 }} />
+    <Skeleton variant="smallText" style={{ width: '90%' }} />
+    <View style={{ height: 4 }} />
+    <Skeleton variant="smallText" style={{ width: '50%' }} />
+    <View style={{ height: 4 }} />
+    <Skeleton variant="smallText" style={{ width: '30%' }} />
+    <View style={{ height: 16 }} />
+    <Skeleton variant="smallText" style={{ height: 250, borderRadius: 8 }} />
+
+    <BButton
+      style={{ marginTop: 16 }}
+      title="Mostrar solicitudes de este hospital"
+      onPress={() => null}
+    />
+    <BButton
+      style={{ marginTop: 16 }}
+      title="Llamar"
+      onPress={() => null}
+      variant="primary-void"
+    />
+    <BButton
+      style={{ marginTop: 16 }}
+      title="Abrir mapas"
+      onPress={() => null}
+      variant="transparent-primary"
+    />
+  </View>
+}
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    padding: 16,
+    marginBottom: 16,
+    marginHorizontal: 16,
+    borderRadius: 8,
+    backgroundColor: ColorsEnum.white,
+    shadowColor: ColorsEnum.darkGray,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 8,
+  },
+  map: {
+    height: 200,
+    width: "100%",
+    borderRadius: 8,
+    marginTop: 16,
+    borderColor: ColorsEnum.primary,
+    borderWidth: 2,
+  }
+})
