114c69e1465311034ec2b1a6b1249fe1c7cb705f
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2023-06-15 21:43:12 -0600

    feat: new button pressed styles

--- src/Components/BButton.tsx
+++ src/Components/BButton.tsx
@@ -1,11 +1,12 @@
 import { ColorsEnum } from "@theme";
 import { FC } from "react";
-import { Pressable, PressableProps, StyleSheet } from "react-native";
+import { Pressable, PressableProps, PressableStateCallbackType, StyleProp, StyleSheet, ViewStyle } from "react-native";
 import { BText, BTextProps } from "./BText";
 
 interface BButtonProps extends PressableProps {
   text?: BTextProps;
   title: string;
+  style?: StyleProp<ViewStyle>,
   variant?:
     | "secondary"
     | "primary"
@@ -44,23 +45,29 @@ export const BButton: FC<BButtonProps> = ({
       break;
   }
 
-  const pressableStyle = StyleSheet.flatten([
+  const pressableStyle: (state: PressableStateCallbackType) => StyleProp<ViewStyle>  = ({ pressed }) => StyleSheet.flatten([
     styles.button,
-    variant === "secondary" && styles.secondaryButton,
-    variant === "primary" && styles.primaryButton,
-    variant === "secondary-void" && styles.secondaryVoid,
-    variant === "primary-void" && styles.primaryVoid,
-    variant === "transparent" && styles.transparentButton,
+    styles[variant],
     style,
+    pressed && pressedStyles[variant],
   ]);
 
   return (
     <Pressable style={pressableStyle} {...rest}>
-      {title && (
-        <BText bold color={textColor} {...text}>
-          {title}
-        </BText>
-      )}
+      {
+        ({ pressed }) => {
+          return title ? (
+            <BText
+              bold
+              color={textColor}
+              {...text}
+              style={pressed && pressedTextStyles[variant]}
+            >
+              {title}
+            </BText>
+          ) : null
+        }
+      }
     </Pressable>
   );
 };
@@ -71,25 +78,76 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 8,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    elevation: 8,
   },
-  primaryButton: {
+  primary: {
     backgroundColor: ColorsEnum.primary,
+    shadowColor: ColorsEnum.darkGray,
   },
-  secondaryButton: {
+  secondary: {
     backgroundColor: ColorsEnum.secondary,
+    shadowColor: ColorsEnum.darkGray,
   },
-  primaryVoid: {
+  "primary-void": {
     borderWidth: 1,
     borderColor: ColorsEnum.primary,
+    shadowColor: ColorsEnum.primary,
   },
-  secondaryVoid: {
+  "secondary-void": {
     borderWidth: 1,
     borderColor: ColorsEnum.secondary,
+    shadowColor: ColorsEnum.secondary,
   },
-  transparentButton: {
+  transparent: {
     backgroundColor: "transparent",
   },
   whiteText: {
+    color: ColorsEnum.white,
+  },
+});
+
+const pressedStyles = StyleSheet.create({
+  primary: {
+    backgroundColor: "transparent",
+    borderColor: ColorsEnum.primary,
+    borderWidth: 1,
+  },
+  secondary: {
+    backgroundColor: "transparent",
+    borderColor: ColorsEnum.secondary,
+    borderWidth: 1,
+  },
+  "primary-void": {
+    backgroundColor: ColorsEnum.primary,
+  },
+  "secondary-void": {
+    backgroundColor: ColorsEnum.secondary,
+  },
+  transparent: {
+    backgroundColor: ColorsEnum.secondary,
+  },
+});
+
+const pressedTextStyles = StyleSheet.create({
+  primary: {
+    color: ColorsEnum.primary,
+  },
+  secondary: {
+    color: ColorsEnum.secondary,
+  },
+  "primary-void": {
+    color: ColorsEnum.white,
+  },
+  "secondary-void": {
+    color: ColorsEnum.white,
+  },
+  transparent: {
     color: ColorsEnum.white,
   },
 });
