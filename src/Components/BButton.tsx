8932d51040a4914dfc7e929862c10a7103792d3f
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2023-06-15 21:07:02 -0600

    fix: style propagable down BButton

--- src/Components/BButton.tsx
+++ src/Components/BButton.tsx
@@ -18,6 +18,7 @@ export const BButton: FC<BButtonProps> = ({
   title,
   text,
   variant = "primary",
+  style,
   ...rest
 }) => {
   let textColor: BTextProps["color"] = "white";
@@ -50,6 +51,7 @@ export const BButton: FC<BButtonProps> = ({
     variant === "secondary-void" && styles.secondaryVoid,
     variant === "primary-void" && styles.primaryVoid,
     variant === "transparent" && styles.transparentButton,
+    style,
   ]);
 
   return (
