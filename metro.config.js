5f039caae84414ce29a98e38e48e708e6aae1f4e
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2023-06-16 01:29:26 -0600

    fix: DatePicker annoying error

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,16 @@ const defaultConfig = getDefaultConfig(__dirname);
 
 defaultConfig.resolver.assetExts.push("cjs");
 
-module.exports = defaultConfig;
+module.exports = {
+  ...defaultConfig,
+  server: {
+    rewriteRequestUrl: (url) => {
+      if (!url.endsWith('.bundle')) {
+        return url;
+      }
+      // https://github.com/facebook/react-native/issues/36794
+      // JavaScriptCore strips query strings, so try to re-add them with a best guess.
+      return url + '?platform=ios&dev=true&minify=false&modulesOnly=false&runModule=true';
+    }, // ...
+  }, // ...
+};
