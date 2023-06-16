306172bed1d331d7efd16195827ce24f46e8df13
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2023-06-16 12:33:30 -0600

    feat: started create a request screen

--- src/Screens/CreateDonationRequest/CreateDonationRequest.tsx
+++ src/Screens/CreateDonationRequest/CreateDonationRequest.tsx
@@ -2,8 +2,9 @@ import { BText } from "@components";
 import { LoadingContext } from "@context";
 import { useAuth, useUser } from "@hooks";
 import React, { FC, useContext, useEffect } from "react";
-import { Image } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { RequestCard } from "../Home/RequestCard";
 
 export const CreateDonationRequest: FC = () => {
   const { user } = useAuth();
@@ -14,6 +15,8 @@ export const CreateDonationRequest: FC = () => {
   } = useUser(user?.uid);
   const { showLoading, hideLoading } = useContext(LoadingContext);
   
+  console.log(userData)
+
   useEffect(() => {
     if(isLoadingUser) {
       showLoading("Cargando información del usuario...")
@@ -29,16 +32,47 @@ export const CreateDonationRequest: FC = () => {
 
   return (
     <SafeAreaView>
-      <BText size="title" bold color="black" style={{ alignSelf: "center" }}>
-        Crear una petición
+      <BText size="title" bold color="secondary" style={{ alignSelf: "center" }}>
+        Crear una solicitud
       </BText>
-      <Image
-        source={{ uri: userData.avatar }}
-        style={{ width: 20, height: 20 }}
-      />
-      <BText size="large" bold>
-        {userData.name}
-      </BText>
+      <View style={styles.container}>
+
+        <BText style={{ alignSelf: 'center', marginBottom: 16 }} color="black">
+          Así verían otros esta solicitud para <BText bold color="secondary">{userData.name}</BText>
+        </BText>
+        <RequestCard
+          setIsContactModalVisible={() => null}
+          requestDonation={{
+            id: "1",
+            name: "Juan Perez",
+            avatar: "https://i.pravatar.cc/300",
+            bloodType: "A+",
+            hospital: "Hospital General",
+            city: "Ciudad de México",
+            description: "Necesito sangre para mi operación",
+            firstname: "Juan",
+            contact: "5512345678",
+            age: 25,
+            litersDonated: 2,
+          }}
+        />
+
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    marginTop: 16,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 8
+  },
+  cardContainer: {
+    flexDirection: "row",
+  }
+});
