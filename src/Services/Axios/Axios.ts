03ff7110518f37d94b6ea18ade0b2c22d01226c8
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2023-06-16 20:59:38 -0600

    feat: axios keys

--- src/Services/Axios/Axios.ts
+++ src/Services/Axios/Axios.ts
@@ -4,6 +4,11 @@ import {
 } from "@env";
 import { URL } from 'react-native-url-polyfill';
 
+export const ApiQueryKeys = {
+  Hospital: "/Hospital",
+  DonationRequest: "/DonationRequest",
+} as const;
+
 export const appAxios = axios.create({
   baseURL: new URL(SERVER_URL).href,
   headers: {
