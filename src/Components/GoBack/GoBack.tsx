08827dc260f9f8dc11af5c0f9ec31d81f277bcd9
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2023-06-16 20:57:10 -0600

    feat: go back floating

--- src/Components/GoBack/GoBack.tsx
+++ src/Components/GoBack/GoBack.tsx
@@ -1,20 +1,44 @@
 import { FC } from "react";
-import { Pressable, View } from "react-native";
+import { Pressable, View, StyleSheet } from "react-native";
 import { useAppNavigation } from "@hooks";
 import { BText } from "../BText";
 import { ArrowLeftSvg } from "@assets";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export const GoBack: FC = () => {
+type GoBackProps = {
+  float?: boolean;
+}
+
+export const GoBack: FC<GoBackProps> = ({
+  float = false
+}) => {
   const { goBack } = useAppNavigation();
+  const { top } = useSafeAreaInsets();
 
   const onGoBack = () => {
     goBack();
   };
+
   return (
-    <Pressable style={{ flexDirection: 'row' }} onPress={onGoBack}>
+    <Pressable style={StyleSheet.flatten([
+      { flexDirection: 'row' },
+      float && {
+        ...styles.float,
+        top: top,
+        left: 16,
+      }
+    ])} onPress={onGoBack}>
       <ArrowLeftSvg />
-      <View  style={{ width: 8 }} />
-      <BText color="secondary" bold>Regresar</BText>
+      <View style={{ width: 8 }} />
+      <BText style={{ lineHeight: 24 }} color="secondary" bold>Regresar</BText>
     </Pressable>
   );
 };
+
+const styles = StyleSheet.create({
+  float: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  }
+})
