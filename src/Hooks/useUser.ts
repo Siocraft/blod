0817ee51ecf093d3d33c3dec2ffddb79a41e2e5c
Santiago González Siordia <santiago.gonzalezsiordia@syngenta.com>
2023-06-25 00:40:31 -0600

    refactor: removed useless code

--- src/Hooks/useUser.ts
+++ src/Hooks/useUser.ts
@@ -1,20 +1,13 @@
 import { QueryKeys } from "@config";
-import { LoadingContext } from "@context";
 import { ErrorReporting, getUser } from "@services";
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
 
 export const useUser = (userId?: string) => {
-  const { showLoading, hideLoading } = useContext(LoadingContext);
 
   return useQuery({
     queryKey: [QueryKeys.USER, userId],
     queryFn: () => {
-      showLoading("Cargando información del usuario");
       return getUser(userId);
-    },
-    onSettled: () => {
-      hideLoading();
     },
     onError: e => {
       ErrorReporting(e);
