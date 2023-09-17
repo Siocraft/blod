20b179e90fee758c4ba91f82f5cfb5b83aa3326f
Santiago Siordia <santiago.gonzalez.siordia@gmail.com>
2023-09-17 16:09:33 -0600

    fix: key correction

--- src/Screens/Home/Requests/Requests.tsx
+++ src/Screens/Home/Requests/Requests.tsx
@@ -50,7 +50,7 @@ export const Requests: FC<RequestsProps> = ({ setIsContactModalVisible }) => {
             />
           )}
           ListFooterComponent={() => <View style={{ height: 80 }} />}
-          keyExtractor={item => "Requests_Donor_card_" + item.id}
+          keyExtractor={item => "Requests_Donor_Card_" + item.id}
           onEndReached={() => fetchNextPage()}
         />
       }
