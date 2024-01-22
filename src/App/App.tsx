8ab27e2495b5b74d6cfd44065d421ca95c3c3e97
Santiago <santiago.gonzalez.siordia@gmail.com>
2024-01-22 10:33:00 -0600

    feat: redux being used in app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,11 +4,13 @@ import { useForegroundPermissions } from "expo-location";
 import React, { useEffect, useState } from "react";
 import { LogBox } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
 import "../Config/firebase";
 import { queryClient } from "../Config/reactQuery";
 import { LoadingContext } from "../Context";
 import { RootStackNavigator } from "../Navigators";
 import { Loading } from "../Screens/Loading";
+import { store } from "../Services/Redux";
 LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
 LogBox.ignoreLogs(["Setting a timer for a long period"]);
 LogBox.ignoreLogs(["Animated: `useNativeDriver` was not specified."]);
@@ -27,29 +29,31 @@ export const App = () => {
   }, [status]);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <SafeAreaProvider>
-          <LoadingContext.Provider
-            value={{
-              loading: isLoading,
-              message: loadingMessage,
-              showLoading: (message: string) => {
-                setIsLoading(true);
-                setIsLoadingMessage(message);
-                return "";
-              },
-              hideLoading: () => {
-                setIsLoading(false);
-                setIsLoadingMessage("");
-              },
-            }}
-          >
-            {isLoading && <Loading />}
-            <RootStackNavigator />
-          </LoadingContext.Provider>
-        </SafeAreaProvider>
-      </NavigationContainer>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <SafeAreaProvider>
+            <LoadingContext.Provider
+              value={{
+                loading: isLoading,
+                message: loadingMessage,
+                showLoading: (message: string) => {
+                  setIsLoading(true);
+                  setIsLoadingMessage(message);
+                  return "";
+                },
+                hideLoading: () => {
+                  setIsLoading(false);
+                  setIsLoadingMessage("");
+                },
+              }}
+            >
+              {isLoading && <Loading />}
+              <RootStackNavigator />
+            </LoadingContext.Provider>
+          </SafeAreaProvider>
+        </NavigationContainer>
+      </QueryClientProvider>
+    </Provider>
   );
 };
