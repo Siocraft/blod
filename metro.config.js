779ae278db6896d292691161e457654bfd2cdafb
Santiago <santiago.gonzalez.siordia@gmail.com>
2024-01-22 10:32:41 -0600

    chore: redux added

--- metro.config.js
+++ metro.config.js
@@ -6,14 +6,17 @@ defaultConfig.resolver.assetExts.push("cjs");
 
 module.exports = {
   ...defaultConfig,
+  resolver: {
+    sourceExts: ["js", "json", "ts", "tsx", "cjs"],
+  },
   server: {
     rewriteRequestUrl: (url) => {
-      if (!url.endsWith('.bundle')) {
+      if (!url.endsWith(".bundle")) {
         return url;
       }
       // https://github.com/facebook/react-native/issues/36794
       // JavaScriptCore strips query strings, so try to re-add them with a best guess.
-      return url + '?platform=ios&dev=true&minify=false&modulesOnly=false&runModule=true';
+      return url + "?platform=ios&dev=true&minify=false&modulesOnly=false&runModule=true";
     }, // ...
   }, // ...
 };
