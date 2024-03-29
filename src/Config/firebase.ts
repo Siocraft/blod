388889b8ebe8df637ce773ddcee6d9bc6c82f45a
Santiago <santiago.gonzalez.siordia@gmail.com>
2024-03-28 21:45:15 -0600

    Remove unnecessary imports and comments from firebase.ts

--- src/Config/firebase.ts
+++ src/Config/firebase.ts
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import {
   FIREBASE_API_KEY,
   FIREBASE_APP_ID,
@@ -12,11 +11,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
@@ -27,7 +22,6 @@ const firebaseConfig = {
   measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firestore = getFirestore(firebaseApp);
 export const firebaseStorage = getStorage(firebaseApp);
